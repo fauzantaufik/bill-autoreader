5aee96b38c00f972491cc671253c73514f01ac1f
fauzantaufik <fauzantaufik178@gmail.com>
2024-06-24 14:38:15 +0700

    [FEAT] Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bill-autoreader",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="A utility to process bill data",
     long_description=open("README.md").read(),
